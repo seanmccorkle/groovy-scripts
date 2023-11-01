4540efde3e7ac171fd5b8e57b910937e8ed0a98c
Sean McCorkle <20233701+seanmccorkle@users.noreply.github.com>
2023-11-01 14:38:32 -0400

    move def outside of node

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,12 @@
 def call() {
+    def command = 'cd ${APP_ORIGIN_WORKSPACE} && find * -maxdepth 0 -type d'
+    def output = ['bash', '-c', command].execute().in.text
+    def directories = output.trim().split('\r?\n')
     node {
         stage("docker build main branch") {
             script {
               sh "echo 'docker build'"
               sh "echo 'APP_ORIGIN_WORKSPACE = ${APP_ORIGIN_WORKSPACE}'"
-              def command = 'cd ${APP_ORIGIN_WORKSPACE} && find * -maxdepth 0 -type d'
-              def output = ['bash', '-c', command].execute().in.text
-              def directories = output.trim().split('\r?\n')
               for( app in directories ) {
                   println app
               }
