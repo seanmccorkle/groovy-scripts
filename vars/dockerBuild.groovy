74d1b33b6e89c4bb0f4eb58f4eaf51054510dc59
Sean McCorkle <20233701+seanmccorkle@users.noreply.github.com>
2023-11-01 11:20:46 -0400

    verify workspace

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,6 +3,7 @@ def call() {
         stage("docker build main branch") {
             script {
               sh "echo 'docker build'"
+              echo "APP_ORIGIN_WORKSPACE = ${APP_ORIGIN_WORKSPACE}"
             }
         }
     }
