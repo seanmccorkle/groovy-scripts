f7b1d9223a2be2e862f64a22364f408675221544
Sean McCorkle <20233701+seanmccorkle@users.noreply.github.com>
2023-11-01 12:34:21 -0400

    discover directories and print

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,6 +4,12 @@ def call() {
             script {
               sh "echo 'docker build'"
               sh "echo 'APP_ORIGIN_WORKSPACE = ${APP_ORIGIN_WORKSPACE}'"
+              def command = 'cd ${APP_ORIGIN_WORKSPACE} && find * -maxdepth 0 -type d'
+              def output = ['bash', '-c', command].execute().in.text
+              def directories = output.trim().split('\r?\n')
+              for( app in directories ) {
+                  println app
+              }
             }
         }
     }
