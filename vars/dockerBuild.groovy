20c02d883e7db2f19959170a6ae71eda51dec5af
Sean McCorkle <20233701+seanmccorkle@users.noreply.github.com>
2023-11-08 13:38:52 -0500

    find docker files

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -6,9 +6,16 @@ def call() {
               sh "echo 'docker build'"
               sh "echo 'APP_ORIGIN_WORKSPACE = ${APP_ORIGIN_WORKSPACE}'"
               if (directories.first() != "") {
+                // there are multiple directories possibly containing Dockerfiles
                 for( app in directories ) {
                   println app
+                  dockerfile = findFiles(glob: '*Dockerfile*')
+                  sh "echo '${dockerfile}'"
                 }
+              } else {
+                  // there are no directories so check for a Dockerfile
+                  dockerfile = findFiles(glob: '*Dockerfile*')
+                  sh "echo '${dockerfile}'"
               }
             }
         }
