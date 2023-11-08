95c3f0f0f37415865cf19fe8be8a304007aec44c
Sean McCorkle <20233701+seanmccorkle@users.noreply.github.com>
2023-11-08 15:51:56 -0500

    check if dockerfile exists

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -5,18 +5,31 @@ def call() {
             script {
               sh "echo 'docker build'"
               sh "echo 'APP_ORIGIN_WORKSPACE = ${APP_ORIGIN_WORKSPACE}'"
-              if (directories.first() != "") {
-                // there are multiple directories possibly containing Dockerfiles
-                for( app in directories ) {
-                  println app
-                  def dockerfile = findFiles(glob: '*Dockerfile*')
-                  sh "echo '${dockerfile}'"
-                }
+              File dockerfile = new File("${APP_ORIGIN_WORKSPACE}/app/Dockerfile")
+
+              if (dockerfile.exists()) {
+                  println("Build container image using Dockerfile")
               } else {
-                  // there are no directories so check for a Dockerfile
-                  def dockerfile = findFiles(glob: '*Dockerfile*')
-                  sh "echo '${dockerfile}'"
+                  println("Check directories for Dockerfiles")
+                  if (directories.first() != "") {
+                      // there are multiple directories possibly containing Dockerfiles
+                      for( app in directories ) {
+                          println app
+                      }
               }
+                
+              //if (directories.first() != "") {
+              //  // there are multiple directories possibly containing Dockerfiles
+              //  for( app in directories ) {
+              //    println app
+              //    def dockerfile = findFiles(glob: '*Dockerfile*')
+              //    sh "echo '${dockerfile}'"
+              //  }
+              //} else {
+              //   // there are no directories so check for a Dockerfile
+              //    def dockerfile = findFiles(glob: '*Dockerfile*')
+              //    sh "echo '${dockerfile}'"
+              //}
             }
         }
     }
