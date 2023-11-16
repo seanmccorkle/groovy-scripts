10e490239174aa6a522d9add68786aa53b13cd33
Sean McCorkle <seanmccorkle@Seans-MBP-2.MG8702>
2023-11-16 13:32:59 -0500

    update shell commands

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,9 +2,9 @@ def call() {
     // check for directories located under app directory 
     def directories = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find * -maxdepth 0 -type d', returnStdout: true).trim().split('\n')
     // check for a single Dockerfile under app directory
-    def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find * -maxdepth 0 -type f | grep -i dockerfile', returnStdout: true).trim().split('\n')
+    def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStatus: true).trim().split('\n')
     // check for Dockerfiles located in nested directories under app directory
-    def dockerfiles = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find * -maxdepth 1 -type f | grep -i dockerfile', returnStdout: true).trim().split('\n')
+    def dockerfiles = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find */Dockerfile', returnStdout: true).trim().split('\n')
     node {
         stage("docker build main branch") {
             script {
@@ -13,8 +13,11 @@ def call() {
 
               //File dockerfile = new File("${APP_ORIGIN_WORKSPACE}/app/Dockerfile")
 
-              if (dockerfile.first() != "") {
+              // check command exit code
+              if (dockerfile == 0) {
                 println("cd ${APP_ORIGIN_WORKSPACE}/app/ && docker build -t app:latest .")
+              } else {
+                println("file ${APP_ORIGIN_WORKSPACE}/app/Dockerfile doesn't exist")
               }
 
               if (directories.first() != "" && dockerfiles.first() != "") {
