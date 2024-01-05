f27e57a7d36e37e088c6b2745ed8e422591aede4
Sean McCorkle <seanmccorkle@Seans-MBP-2.MG8702>
2024-01-05 11:21:24 -0500

    revert to previous command for dockerfile variable

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,8 +2,8 @@ def call() {
     // check for directories located under app directory 
     def directories = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find * -maxdepth 0 -type d', returnStdout: true).trim().split('\n')
     // check for a single Dockerfile under app directory
-    //def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStatus: true)
-    def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStdout: true).trim().split('\n')
+    def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStatus: true)
+    //def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStdout: true).trim().split('\n') //ERROR: script returned exit code 1
     // check for Dockerfiles located in nested directories under app directory
     def dockerfiles = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find */Dockerfile', returnStdout: true).trim().split('\n')
     node {
