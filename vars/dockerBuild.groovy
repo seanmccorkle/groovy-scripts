64a1df9f325e4394bad27930433bd5fc2f0804ff
Sean McCorkle <seanmccorkle@Seans-MBP-2.MG8702>
2024-01-05 11:38:04 -0500

    testing command for dockerfile variable

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,9 +2,9 @@ def call() {
     // check for directories located under app directory 
     def directories = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find * -maxdepth 0 -type d', returnStdout: true).trim().split('\n')
     // check for a single Dockerfile under app directory
-    //def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStatus: true) //find: Dockerfile: No such file or directory
+    def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStatus: true) //find: Dockerfile: No such file or directory
     //def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStdout: true).trim().split('\n') //ERROR: script returned exit code 1
-    def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStdout: true)
+    //def dockerfile = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find Dockerfile', returnStdout: true) //ERROR: script returned exit code 1
     // check for Dockerfiles located in nested directories under app directory
     def dockerfiles = sh(script: 'cd ${APP_ORIGIN_WORKSPACE}/app && find */Dockerfile', returnStdout: true).trim().split('\n')
     node {
@@ -21,7 +21,8 @@ def call() {
               //} else {
               //  println("file ${APP_ORIGIN_WORKSPACE}/app/Dockerfile doesn't exist")
               //}
-              println(dockerfile)
+              //println(dockerfile)
+              sh "echo 'dockerfile: ${dockerfile}'"
 
               if (directories.first() != "" && dockerfiles.first() != "") {
                   // there are at least one or more directories and Dockerfiles
